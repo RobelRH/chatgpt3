6ce5e6eba7f29d77a0df0d170d68926cd7f3cb88
RobelRH <robel.redda28@gmail.com>
2023-01-18 21:41:00 +0300

    chat interface ready

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,12 +1,20 @@
 import './style_form.css'
 import Send from '../../assets/send.svg'
+import { useState } from 'react'
 
 function Form() {
 
+
+    const [inputPrompt, setInputPrompt] = useState("");
+
+    const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
     return (
         <div>
-            <form>
-                <textarea name="prompt" rows={1} cols={1} placeholder="Ask codex..."></textarea>
+            <form onSubmit={handleSubmit}>
+                <textarea name="prompt" onChange={(e) => setInputPrompt(e.target.value)} rows={1} cols={1} placeholder="ask chat gpt ..."></textarea>
                 <button type="submit"><img src={Send} alt="send" /></button>
             </form>
         </div>
